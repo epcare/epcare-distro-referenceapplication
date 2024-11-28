0db0accc74ba15c77b1cac1fb1e64e733a1d8eed
jabahum <jabajnan8@gmail.com>
2024-11-28 13:14:03 +0300

    update build platform

--- dockerhub.sh
+++ dockerhub.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # automate tagging with the short commit hash
-docker build --platform="linux/amd64" --no-cache -t jabahum/ugandaemr-distro:$(git rev-parse --short HEAD) .
+docker build --no-cache -t jabahum/ugandaemr-distro:$(git rev-parse --short HEAD) .
 docker tag jabahum/ugandaemr-distro:$(git rev-parse --short HEAD) jabahum/ugandaemr-distro
 docker push jabahum/ugandaemr-distro:$(git rev-parse --short HEAD)
 docker push jabahum/ugandaemr-distro:latest
--- frontend/dockerhub.sh
+++ frontend/dockerhub.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # automate tagging with the short commit hash
-docker build --platform="linux/amd64" --no-cache -t jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD) .
+docker build  --no-cache -t jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD) .
 docker tag jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD) jabahum/ugandaemr-frontend
 docker push jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD)
 docker push jabahum/ugandaemr-frontend:latest
