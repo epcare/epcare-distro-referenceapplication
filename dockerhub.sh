e4d84aaff138435038d81a0c084716f289db4b74
jabahum <jabajnan8@gmail.com>
2025-02-05 21:20:07 +0300

    remove unused code and scripts

--- dockerhub.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# automate tagging with the short commit hash
-docker build --no-cache -t jabahum/ugandaemr-distro:$(git rev-parse --short HEAD) .
-docker tag jabahum/ugandaemr-distro:$(git rev-parse --short HEAD) jabahum/ugandaemr-distro
-docker push jabahum/ugandaemr-distro:$(git rev-parse --short HEAD)
-docker push jabahum/ugandaemr-distro:latest
--- frontend/dockerhub.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# automate tagging with the short commit hash
-docker build  --no-cache -t jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD) .
-docker tag jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD) jabahum/ugandaemr-frontend
-docker push jabahum/ugandaemr-frontend:$(git rev-parse --short HEAD)
-docker push jabahum/ugandaemr-frontend:latest
